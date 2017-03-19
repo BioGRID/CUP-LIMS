6e22120cdeb5f66b148072dda8d835451f7bedf6
starkfree <starkfree@gmail.com>
2017-03-19 01:02:21 -0400

    Added Tool for Parsing in Entrez Gene, Resolved #52

--- operations/Annotation/classes/Lookups.py
+++ operations/Annotation/classes/Lookups.py
@@ -10,35 +10,26 @@ class Lookups( ) :
 		self.db = db
 		self.cursor = self.db.cursor( )
 		
-	def buildSGRNAHash( self ) :
+	def buildExistingEntrezGeneHash( self, taxID ) :
+	
+		"""Build a set of entrez genes that are already entered"""
 		
-		"""Build a set of sgRNAs mapped to sgRNA IDs"""
-		mapping = { }
-		self.cursor.execute( "SELECT sgrna_id, sgrna_sequence FROM " + Config.DB_MAIN + ".sgRNAs" )
-		
-		for row in self.cursor.fetchall( ) :
-			mapping[str(row['sgrna_sequence'])] = str(row['sgrna_id'])
-			
-		return mapping
-		
-	def buildSGRNAGroupReferenceHash( self ) :
-		
-		"""Build a set of sgRNA Group References Mapped to Group IDs"""
-		mapping = { }
-		self.cursor.execute( "SELECT sgrna_group_reference_original, sgrna_group_id FROM " + Config.DB_MAIN + ".sgRNA_groups" )
-		
-		for row in self.cursor.fetchall( ) :
-			mapping[str(row['sgrna_group_reference_original'])] = str(row['sgrna_group_id'])
-			
-		return mapping
-		
-	def buildSGRNAToGroupMapping( self ) :
-		
-		"""Build a set of sgRNA to Group Mappings"""
 		mapping = set( )
-		self.cursor.execute( "SELECT sgrna_group_id, sgrna_id FROM " + Config.DB_MAIN + ".sgRNA_group_mappings" )
+		self.cursor.execute( "SELECT entrez_gene_id FROM " + Config.DB_MAIN + ".genes WHERE organism_id=%s", [taxID] )
 		
 		for row in self.cursor.fetchall( ) :
-			mapping.add( str(row['sgrna_group_id']) + "|" + str(row['sgrna_id']) )
+			mapping.add( str(row['entrez_gene_id']) )
+			
+		return mapping
+		
+	def buildEntrezGeneToBioGRIDHash( self, taxID ) :
+	
+		"""Build a set of biogrid gene ids that map to an organism"""
+		
+		mapping = { }
+		self.cursor.execute( "SELECT gene_id, quick_identifier_value FROM " + Config.DB_QUICK + ".quick_identifiers WHERE quick_identifier_type='ENTREZ_GENE' AND organism_id=%s", [taxID] )
+		
+		for row in self.cursor.fetchall( ) :
+			mapping[str(row['quick_identifier_value'])] = str(row['gene_id'])
 			
 		return mapping
